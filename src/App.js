fc69535b621413dd7b2c5df92ddf8ec570ecca23
mohamed-aje <chochoaj93@gmail.com>
2023-02-08 13:44:50 +0200

    latest changes

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "eee72ea1-4850-4160-8d26-d60b552fa91b";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
